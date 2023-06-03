01544e11d72dbac2a4f542d098df762b442eb694
Kolomiiets Lidiia <kolomieclidia7@gmail.com>
2023-06-03 20:58:09 +0300

    complete task number six

---
+++ index.js
@@ -0,0 +1,8 @@
+const numbers = [5, 8, -1, '-6', 11, 'ololo', '-2']
+let sum = 0
+for (let i = 0; i < numbers.length; i++) {
+   if(numbers[i] = (+numbers[i]))
+  sum = sum + numbers[i]
+}
+
+alert('Sum is ' + sum)
