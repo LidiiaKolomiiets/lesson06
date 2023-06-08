079b2cf10fc8bfcbf37e7f7da5d67d95039bfbda
Kolomiiets Lidiia <kolomieclidia7@gmail.com>
2023-06-08 11:18:25 +0300

    fix comments

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const numbers = [5, 8, -1, '-6', 11, 'ololo', '-2']
-let sum = 0
+let sum = 0;
 for (let i = 0; i < numbers.length; i++) {
-   if(numbers[i] = (+numbers[i]))
-  sum = sum + numbers[i]
+  
+  const numbersNew = +numbers[i];
+  
+  if (!isNaN(numbersNew)) {
+    
+    sum += numbersNew;
+  }
 }
 
 alert('Sum is ' + sum)
